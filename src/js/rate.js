2a36e3077ec497ffba5692b137447743c21826bf
Denis Rudov <denis.rudov@gmail.com>
2016-10-18 22:48:25 +0300

    initial commit

---
+++ src/js/rate.js
@@ -0,0 +1,36 @@
+import '../styles/rating.scss';
+
+window.onload = ()=> {
+  let className = 'rating-item';
+  let rating = document.getElementsByClassName('rating')[ 0 ];
+
+  if (rating !== undefined) {
+    rating.addEventListener('click', (e)=> {
+      e.stopPropagation();
+
+      doAjaxCall('Rated...').then(d=> {console.log(d[ 0 ])});
+
+      const li = e.target;
+      const selectedValue = li.dataset.value;
+      const elements = rating.querySelectorAll(`.${className}`);
+
+      for (let i = 0; i < elements.length; i++) {
+        let el = elements[ i ];
+        if (el.dataset.value <= selectedValue) {
+          el.className = `${className} rate`;
+        } else {
+          el.className = className;
+        }
+      }
+
+    });
+  }
+};
+
+function doAjaxCall (...data) {
+  return new Promise((success)=> {
+    setTimeout(()=> {
+      success(data);
+    }, 1000);
+  })
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var Unminified = require('unminified-webpack-plugin');
+var webpack = require('webpack');
+
+var extractSass = new ExtractTextPlugin('stylesheets/[name].css');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    loaders: [
+
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.s?css$/,
+        loader: extractSass.extract('style', [ 'css?root=.', 'sass' ], {
+          publicPath: '../'
+        })
+      },
+      {
+        test: /\.png|jpg|gif$/,
+        loader: 'url?limit=10000&name=images/[name].[ext]'
+      }
+
+    ]
+  },
+  plugins: [
+    extractSass,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unsafe: true,
+        drop_console: true,
+        warnings: false
+      }
+    })
+  ]
+};
