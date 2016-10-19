3d8e0e5c5466415d95c9669268c55949c8d2849a
Denis Rudov <denis.rudov@gmail.com>
2016-10-19 07:18:22 +0300

    support multiple ratings

--- src/js/rate.js
+++ src/js/rate.js
@@ -2,17 +2,18 @@ import '../styles/rating.scss';
 
 window.onload = ()=> {
   let className = 'rating-item';
-  let rating = document.getElementsByClassName('rating')[ 0 ];
+  let rating = document.getElementsByClassName('rating');
 
-  if (rating !== undefined) {
-    rating.addEventListener('click', (e)=> {
+  for (let r = 0; r < rating.length; r++) {
+    let ratingElement = rating[r];
+    ratingElement.addEventListener('click', (e)=> {
       e.stopPropagation();
 
       doAjaxCall('Rated...').then(d=> {console.log(d[ 0 ])});
 
       const li = e.target;
       const selectedValue = li.dataset.value;
-      const elements = rating.querySelectorAll(`.${className}`);
+      const elements = ratingElement.querySelectorAll(`.${className}`);
 
       for (let i = 0; i < elements.length; i++) {
         let el = elements[ i ];
